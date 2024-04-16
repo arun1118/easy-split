1914962b6c689285eaa343e1fa3ab94a5153117a
arun1118 <arunpanigrahi18@gmail.com>
2024-04-17 00:48:13 +0530

    add routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Member from './pages/Member'
+import Bill from './pages/Bill'
+import Result from './pages/Result'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Member />}/>
+          <Route path="/bill" element={<Bill />}/>
+          <Route path="/result" element={<Result />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
