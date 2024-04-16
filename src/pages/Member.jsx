1990c489b1951112ff9756045acb4e0c6d1c22d7
arun1118 <arunpanigrahi18@gmail.com>
2024-04-17 00:53:43 +0530

    basic form in member page

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Member = () => {
+
+  const [name, setName] = useState("")
+
+  const handleChange = (e)=>{
+    setName(e.target.value)
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log("to add ",name)
+    setName("")
+  }
+
   return (
-    <div>Member</div>
+    <div>
+      <form method="POST" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter member name</label>
+        <input type="text" id="name" value={name} onChange={handleChange} placeholder='member name'/>
+
+        <button type="submit">Add</button>
+      </form>
+    </div>
   )
 }
 
