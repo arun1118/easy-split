c8a1dd3041972c4890b0712aa6c609e8b5f85aab
arun1118 <arunpanigrahi18@gmail.com>
2024-04-17 23:32:04 +0530

    add predefined names to add names easily

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -10,6 +10,19 @@ const Member = () => {
   const dispatch = useDispatch()
 
   const [name, setName] = useState("")
+  const [preDefinedNames, setPreDefinedNames] = useState([
+    {"name": "isha", "id": "1", "isChecked": false},
+    {"name": "arun", "id": "2", "isChecked": false},
+    {"name": "ashu", "id": "3", "isChecked": false},
+    {"name": "tej", "id": "4", "isChecked": false},
+    {"name": "chocho", "id": "5", "isChecked": false},
+    {"name": "aswin", "id": "6", "isChecked": false},
+    {"name": "sagar", "id": "7", "isChecked": false},
+    {"name": "parth", "id": "8", "isChecked": false},
+    {"name": "vedant", "id": "9", "isChecked": false},
+    {"name": "tosif", "id": "10", "isChecked": false},
+    {"name": "anushka", "id": "11", "isChecked": false},
+  ])
 
   const handleChange = (e)=>{
     setName(e.target.value)
@@ -25,8 +38,53 @@ const Member = () => {
     dispatch(deleteMember(memberId))
   }
 
+  const handleSelectName = (idx)=>{
+    setPreDefinedNames((prevValue)=>{
+      let updatedValue = [...prevValue]
+      updatedValue[idx]["isChecked"] = !updatedValue[idx]["isChecked"]
+      return updatedValue
+    })
+  }
+
+  const handleAddNames = ()=>{
+    let selectedNamesData = preDefinedNames.filter((name)=> name["isChecked"])
+    let selectedNames = selectedNamesData.map((name)=> name["name"])
+    
+    selectedNames.forEach((name)=>{ dispatch(addMember({"id": nanoid(10), "name":name})) })
+
+    setPreDefinedNames([
+      {"name": "isha", "id": "1", "isChecked": false},
+      {"name": "arun", "id": "2", "isChecked": false},
+      {"name": "ashu", "id": "3", "isChecked": false},
+      {"name": "tej", "id": "4", "isChecked": false},
+      {"name": "chocho", "id": "5", "isChecked": false},
+      {"name": "aswin", "id": "6", "isChecked": false},
+      {"name": "sagar", "id": "7", "isChecked": false},
+      {"name": "parth", "id": "8", "isChecked": false},
+      {"name": "vedant", "id": "9", "isChecked": false},
+      {"name": "tosif", "id": "10", "isChecked": false},
+      {"name": "anushka", "id": "11", "isChecked": false},
+    ])
+  }
+
   return (
     <div>
+      <div>
+        <ul>
+        {
+          preDefinedNames.map((name,idx)=>{
+            return(
+              <li key={idx}>
+              <input type="checkbox" id={`checkbox_${idx}`} checked={name["isChecked"]} onChange={()=> handleSelectName(idx)}/>
+              <label htmlFor={`checkbox_${idx}`}>{name["name"]}</label>
+              </li>
+            )
+          })
+        }
+        </ul>
+        <button type="button" onClick={handleAddNames}>Add</button>
+      </div>
+
       <form method="POST" onSubmit={handleSubmit}>
         <label htmlFor="name">Enter member name</label>
         <input type="text" id="name" value={name} onChange={handleChange} placeholder='member name'/>
